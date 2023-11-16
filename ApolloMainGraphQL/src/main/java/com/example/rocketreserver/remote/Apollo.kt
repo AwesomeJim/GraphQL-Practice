3a40c67a8584e1b86ac8182df267248d3389af41
AwesomeJim <8529047+AwesomeJim@users.noreply.github.com>
2023-11-16 18:21:08 +0300

    configured subscription mutations

--- ApolloMainGraphQL/src/main/java/com/example/rocketreserver/remote/Apollo.kt
+++ ApolloMainGraphQL/src/main/java/com/example/rocketreserver/remote/Apollo.kt
@@ -1,8 +1,10 @@
 package com.example.rocketreserver.remote
 
+import android.util.Log
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.network.okHttpClient
 import com.example.rocketreserver.data.TokenRepository
+import kotlinx.coroutines.delay
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 
@@ -12,11 +14,17 @@ import okhttp3.Response
 val apolloClient = ApolloClient.Builder()
 
     .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com/graphql")
+    .webSocketServerUrl("wss://apollo-fullstack-tutorial.herokuapp.com/graphql")
     .okHttpClient(
         OkHttpClient.Builder()
             .addInterceptor(AuthorizationInterceptor())
             .build()
     )
+    .webSocketReopenWhen { throwable, attempt ->
+        Log.d("Apollo", "WebSocket got disconnected, reopening after a delay", throwable)
+        delay(attempt * 1000)
+        true
+    }
     .build()
 
 
